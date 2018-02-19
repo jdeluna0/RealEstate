29a333ad17290f6d034d59ac840fdb519011bfc5
James <james@owncloud>
2018-02-18 19:51:13 -0600

    bug fix

--- zillow/prop_data.py
+++ zillow/prop_data.py
@@ -3,6 +3,7 @@ import urllib
 import csv
 from bs4 import BeautifulSoup
 import pandas as pd
+import sys
 
 data_file = 'cur_for.txt'
 getComp_url = 'http://www.zillow.com/webservice/GetComps.htm'
@@ -38,9 +39,10 @@ class zillow_property_info:
             url = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id={0}&{1}&{2}'.format(self.zws_id,address,citystatezip)
             r = requests.post(url)
             soup = BeautifulSoup(r.content, 'html.parser')
+            if soup.code.string != '0':
+	        print soup.text
             try:
-              #  info_list.append({'Address':soup.address.string,'CSZ':soup.citystatezip.string,'zpid':soup.zpid.string,'zestimate':'{0:,}'.format(int(soup.zestimate.amount.string))})
-                info_list.append({'Address':soup.address.string,'CSZ':soup.citystatezip.string,'zpid':soup.zpid.string,'zestimate':soup.zestimate.amount.string})
+                info_list.append({'Address':soup.address.string,'CSZ':soup.citystatezip.string,'zpid':soup.zpid.string,'zestimate':'{0:,}'.format(int(soup.zestimate.amount.string))})
             except:
                 info_list.append({'Address':house[0],'CSZ':in_csz,'zpid':'NONE','zestimate':'NONE'})
         df = pd.DataFrame(info_list)
@@ -62,17 +64,26 @@ class zillow_property_info:
         comps = []
         details = []
         for zpid in df_data['zpid']:
-            data = {'zws-id':self.zws_id,'zpid':zpid, 'count':1}
-            soup = self.send_request(getDeepComp_url,data)
+        #    data = {'zws-id':self.zws_id,'zpid':zpid, 'count':1}
+	    zpid = urllib.urlencode({'zpid':zpid})
+	    count = urllib.urlencode({'count':1})
+            url = 'http://www.zillow.com/webservice/GetDeepComps.htm?zws-id={0}&{1}&{2}'.format(self.zws_id,zpid,1)
+            #soup = self.send_request(getDeepComp_url,data)
+            #soup = self.send_request(getDeepComp_url,data)
+	    r = request.post(url)
+	    soup = BeautifulSoup(r.content, 'html.parser')
+            if soup.code.string != '0':
+	        print soup.text
             try:
                comps.append("{0}; EST: {1:,}".format(soup.properties.comp.address.street.string,int(soup.properties.comp.zestimate.amount.string)))
             except:
                 comps.append("None Found")
             try:
-                details.append({'Year Built':soup.yearbuilt.string,'SQ FT':soup.finishedsqft.string,'Baths':soup.bathrooms.string,'Beds':soup.bedrooms.string,'Tax Assess':'{0:,}'.format(int(soup.taxassessment.string)),'Tax Assess Year':soup.taxassessmentyear.string})
+           	details.append({'Year Built':soup.yearbuilt.string,'SQ FT':soup.finishedsqft.string,'Baths':soup.bathrooms.string,'Beds':soup.bedrooms.string,'Tax Assess':"{0:,}".format(float(soup.taxassessment.string)),'Tax Assess Year':soup.taxassessmentyear.string})
             except:
+		e = sys.exc_info()[0]
+		print "Error: {0}".format(e)
                 details.append({'Year Built':'None','SQ FT':'None','Baths':'None','Beds':'None','Tax Assess':'None','Tax Assess Year':'None'})
-        #return details
         df_data['Comparison'] = comps
         df_data = pd.concat([df_data,pd.DataFrame(details)], axis=1)
         return df_data
@@ -84,4 +95,4 @@ data_list = p.parse_data_file(data_file)
 
 #df_data = p.getSearch(data_list)
 #deets = p.getDeepComps(df_data)
-df_data = p.getDeepComps(p.getSearch(data_list))
+data = p.getDeepComps(p.getSearch(data_list))
